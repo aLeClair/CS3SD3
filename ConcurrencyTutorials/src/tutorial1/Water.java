e9d1e6185de20486bfa4fa0ef64a327d135b0e99
aLeClair <aleclair123@gmail.com>
2016-09-26 01:24:32 -0400

    Cleaned Water Example

--- ConcurrencyTutorials/src/tutorial1/Water.java
+++ ConcurrencyTutorials/src/tutorial1/Water.java
@@ -3,39 +3,21 @@ package tutorial1;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
-class numHydOxy{
-	private int numberH, numberO;
-	public numHydOxy(){
-		numberH = 0;
-		numberO = 0;
-	}
-	
-	public int getH(){
-		return numberH;
-	}
-	public void incH(){
-		numberH++;
-	}
-	
-	public int getO(){
-		return numberO;
-	}
-	public void incO(){
-		numberO++;
-	}
-}
 
 class Hydrogen implements Runnable{
 	public void run(){
 		synchronized (Water.numOxygen){
-		synchronized (Water.numHydrogen) {
+			synchronized (Water.numHydrogen) {
+				Water.numOfHydrogen++;
 				Water.numHydrogen.release();
-				System.out.println("Hydrogen made!");
+				System.out.println("There are currently " + Water.numOfHydrogen + " Hydrogen atoms!");
 				try{
-					if((Water.numHydrogen.availablePermits() > 0) && (Water.numHydrogen.availablePermits() % 2 == 0) && (Water.numOxygen.availablePermits() > 0)){
-						System.out.println("A water is made!");
-						Water.NUM_WATER--;
+					if((Water.numHydrogen.availablePermits() > 1) && (Water.numOxygen.availablePermits() > 0)){
+						System.out.println("We have made " + Water.NUM_WATER + " Water molecules!");
+						Water.NUM_WATER++;
 						Water.numHydrogen.acquire(2);
+						Water.numOfHydrogen = Water.numOfHydrogen - 2;
+						Water.numOfOxygen--;
 						Water.numOxygen.acquire();	
 					}
 				} catch (InterruptedException e) {
@@ -54,8 +36,26 @@ class Hydrogen implements Runnable{
 
 class Oxygen implements Runnable{
 	public void run(){
+		synchronized (Water.numOxygen){
 			Water.numOxygen.release();
-			System.out.println("Oxygen made!");
+			Water.numOfOxygen++;
+			System.out.println("There are currently " + Water.numOfOxygen + " Oxygen atoms!");
+				synchronized (Water.numHydrogen) {
+					try{
+						if(Water.numHydrogen.availablePermits() > 1){
+							System.out.println("We have made " + Water.NUM_WATER + " Water molecules!");
+							Water.NUM_WATER++;
+							Water.numHydrogen.acquire(2);
+							Water.numOfHydrogen = Water.numOfHydrogen - 2;
+							Water.numOfOxygen--;
+							Water.numOxygen.acquire();	
+						}
+					} catch (InterruptedException e) {
+							//TO DO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
 		
 	}
 
@@ -67,9 +67,9 @@ class Oxygen implements Runnable{
 
 public class Water {
 
-	public static int NUM_WATER = 20;
-	static numHydOxy num;
+	public static int NUM_WATER = 0;
     public static Semaphore numHydrogen, numOxygen;
+    public static int numOfHydrogen, numOfOxygen;
 	
 	public Water(){
 		numHydrogen = new Semaphore(0);
@@ -80,7 +80,7 @@ public class Water {
 		// TODO Auto-generated method stub
 		Water water = new Water();
 		Random randomElement = new Random();
-		while (NUM_WATER > 0){
+		while (NUM_WATER < 20){
 			int n = randomElement.nextInt(30);
 			if (n % 3 == 0){
 				Oxygen ThreadO = new Oxygen();
